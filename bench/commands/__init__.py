0ac008c940cf705bac09c750ae84f3e1d82726bc
Rohan <rohan@digithinkit.com>
2019-08-17 02:39:49 +0530

    fix: use python version number for migrating env

--- bench/commands/__init__.py
+++ bench/commands/__init__.py
@@ -1,187 +1,157 @@
+import logging
+import shutil
+from datetime import datetime
+from pathlib import Path
+from urllib.parse import urlparse
+
 import click
 
-import os, shutil
-import os.path as osp
-import logging
-
-from datetime import datetime
-
-from bench.utils import which, exec_cmd
+import bench
+from bench.commands.config import config
+from bench.commands.git import remote_reset_url, remote_set_url, remote_urls
+from bench.commands.install import install
+from bench.commands.make import (exclude_app_for_update, get_app,
+	include_app_for_update, init, new_app, remove_app)
+from bench.commands.setup import setup
+from bench.commands.update import retry_upgrade, switch_to_branch, update
+from bench.commands.utils import (backup_all_sites, backup_site, bench_src,
+	disable_production, download_translations, prepare_beta_release, release,
+	renew_lets_encrypt, restart, set_default_site, set_mariadb_host,
+	set_nginx_port, set_ssl_certificate, set_ssl_certificate_key,
+	set_url_root, shell, start)
+from bench.config.common_site_config import get_config
+from bench.utils import exec_cmd, setup_logging, which
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
+
 
 def print_bench_version(ctx, param, value):
-	"""Prints current bench version"""
+	"Prints current bench version"
+
 	if not value or ctx.resilient_parsing:
 		return
 
-	import bench
 	click.echo(bench.__version__)
 	ctx.exit()
 
+
 @click.group()
-@click.option('--version', is_flag=True, is_eager=True, callback=print_bench_version, expose_value=False)
+@click.option('--version', is_flag=True, callback=print_bench_version)
 def bench_command(bench_path='.'):
-	"""Bench manager for Frappe"""
-	import bench
-	from bench.utils import setup_logging
+	"Bench manager for Frappe"
 
-	bench.set_frappe_version(bench_path=bench_path)
-	setup_logging(bench_path=bench_path)
+	bench.set_frappe_version(bench_path)
+	setup_logging(bench_path)
 
-
-from bench.commands.make import init, get_app, new_app, remove_app
-bench_command.add_command(init)
-bench_command.add_command(get_app)
-bench_command.add_command(new_app)
-bench_command.add_command(remove_app)
-
-
-from bench.commands.update import update, retry_upgrade, switch_to_branch, switch_to_master, switch_to_develop
-bench_command.add_command(update)
-bench_command.add_command(retry_upgrade)
-bench_command.add_command(switch_to_branch)
-bench_command.add_command(switch_to_master)
-bench_command.add_command(switch_to_develop)
-
-from bench.commands.utils import (start, restart, set_nginx_port, set_ssl_certificate, set_ssl_certificate_key, set_url_root,
-	set_mariadb_host, set_default_site, download_translations, shell, backup_site, backup_all_sites, release, renew_lets_encrypt,
-	disable_production, bench_src, prepare_beta_release)
-bench_command.add_command(start)
-bench_command.add_command(restart)
-bench_command.add_command(set_nginx_port)
-bench_command.add_command(set_ssl_certificate)
-bench_command.add_command(set_ssl_certificate_key)
-bench_command.add_command(set_url_root)
-bench_command.add_command(set_mariadb_host)
-bench_command.add_command(set_default_site)
-bench_command.add_command(download_translations)
-bench_command.add_command(shell)
-bench_command.add_command(backup_site)
-bench_command.add_command(backup_all_sites)
-bench_command.add_command(release)
-bench_command.add_command(renew_lets_encrypt)
-bench_command.add_command(disable_production)
-bench_command.add_command(bench_src)
-bench_command.add_command(prepare_beta_release)
-
-from bench.commands.setup import setup
-bench_command.add_command(setup)
-
-
-from bench.commands.config import config
-bench_command.add_command(config)
-
-from bench.commands.git import remote_set_url, remote_reset_url, remote_urls
-bench_command.add_command(remote_set_url)
-bench_command.add_command(remote_reset_url)
-bench_command.add_command(remote_urls)
-
-from bench.commands.install import install
-bench_command.add_command(install)
-
-from bench.config.common_site_config import get_config
-try:
-	from urlparse 	  import urlparse
-except ImportError:
-	from urllib.parse import urlparse
 
 @click.command('migrate-env')
-@click.argument('python', type = str)
-@click.option('--no-backup', is_flag=True)
-def migrate_env(python, no_backup = False):
-	"""
-	Migrate Virtual Environment to desired Python Version.
-	"""
-	try:
-		# Clear Cache before Bench Dies.
-		config = get_config(bench_path = os.getcwd())
-		rredis = urlparse(config['redis_cache'])
+@click.argument('python', type=float)
+@click.option('--no-backup', is_flag=True, help='Do not backup the existing Virtual Environment')
+def migrate_env(version, no_backup=False):
+	"Migrate Virtual Environment to the desired Python Version"
 
-		redis  = '{redis} -p {port}'.format(
-			redis = which('redis-cli'),
-			port  = rredis.port
-		)
+	# This is with the assumption that a bench is set-up within path
+	path = Path.cwd()
+
+	# Validate migration params
+	python = f"python{version}"
+	python = which(python)
+	if not python:
+		log.error('Invalid Python version...')
+		return
+	virtualenv = which('virtualenv')
+	if not virtualenv:
+		log.error('Invalid virtualenv version...')
+		return
+	pvenv = path.joinpath('env')
+
+	try:
+		# clear cache before bench dies
+		config = get_config(bench_path=path)
+		rredis = urlparse(config['redis_cache'])
+		redis = f"{which('redis-cli')} -p {rredis.port}"
 
 		log.debug('Clearing Redis Cache...')
-		exec_cmd('{redis} FLUSHALL'.format(redis = redis))
-		log.debug('Clearing Redis DataBase...')
-		exec_cmd('{redis} FLUSHDB'.format(redis = redis))
+		exec_cmd('{redis} FLUSHALL'.format(redis=redis))
+		log.debug('Clearing Redis Database...')
+		exec_cmd('{redis} FLUSHDB'.format(redis=redis))
 	except Exception:
 		log.warn('Please ensure Redis Connections are running or Daemonized.')
 
 	try:
-		# This is with the assumption that a bench is set-up within path.
-		path       = os.getcwd()
-
-		# I know, bad name for a flag. Thanks, Ameya! :| - <achilles@frappe.io>
 		if not no_backup:
-			# Back, the f*ck up.
-			parch = osp.join(path, 'archived_envs')
-			if not osp.exists(parch):
-				os.mkdir(parch)
+			parch = Path(path, 'archived_envs')
+			parch.mkdir(parents=True, exist_ok=True)
 
 			# Simply moving. Thanks, Ameya.
 			# I'm keen to zip.
-			source = osp.join(path, 'env')
+			source = path.joinpath('env')
 			target = parch
 
 			log.debug('Backing up Virtual Environment')
-			stamp  = datetime.now().strftime('%Y%m%d_%H%M%S')
-			dest   = osp.join(path, str(stamp))
+			stamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+			dest = path.joinpath(str(stamp))
 
 			# WARNING: This is an archive, you might have to use virtualenv --relocate
 			# That's because virtualenv creates symlinks with shebangs pointing to executables.
 			# shebangs, shebangs - ricky martin.
 
 			# ...and shutil.copytree is a f*cking mess.
-			os.rename(source, dest)
-			shutil.move(dest, target)
+			source.rename(dest)
+			shutil.move(str(dest), str(target))
 
-		log.debug('Setting up a New Virtual {python} Environment'.format(
-			python = python
-		))
+		log.debug(f"Setting up a New Virtual {python} Environment")
+		exec_cmd(f"{virtualenv} --python {python} {pvenv}", cwd=path)
 
-		# Path to Python Executable (Basically $PYTHONPTH)
-		python     = which(python)
-
-
-		virtualenv = which('virtualenv')
-
-		nvenv      = 'env'
-		pvenv      = osp.join(path, nvenv)
-
-		exec_cmd('{virtualenv} --python {python} {pvenv}'.format(
-			virtualenv = virtualenv,
-			python     = python,
-			pvenv      = pvenv
-		), cwd = path)
-
-		pip = osp.join(pvenv, 'bin', 'pip')
-		# exec_cmd('{pip} install --upgrade pip'.format(pip=pip))
-		exec_cmd('{pip} install --upgrade setuptools'.format(pip=pip))
+		pip = Path(pvenv, 'bin', 'pip')
+		exec_cmd(f"{pip} install --upgrade pip")
+		exec_cmd(f"{pip} install --upgrade setuptools")
 		# TODO: Options
 
-		papps  = osp.join(path, 'apps')
-		apps   = ['frappe', 'erpnext'] + [app for app in os.listdir(papps) if app not in ['frappe', 'erpnext']]
+		papps = path.joinpath('apps')
+		apps = [papp for papp in papps.iterdir() if papp.is_dir()]
 
 		for app in apps:
-			papp = osp.join(papps, app)
-			if osp.isdir(papp) and osp.exists(osp.join(papp, 'setup.py')):
-				exec_cmd('{pip} install -e {app}'.format(
-					pip = pip, app = papp
-				))
+			if app.joinpath('setup.py').exists():
+				exec_cmd(f"{pip} install -e {app}")
 
-		log.debug('Migration Successful to {python}'.format(
-			python = python
-		))
+		log.debug(f"Migration Successful to python{version}")
 	except:
 		log.debug('Migration Error')
 		raise
 
-bench_command.add_command(migrate_env)
 
-from bench.commands.make import exclude_app_for_update, include_app_for_update
+bench_command.add_command(backup_all_sites)
+bench_command.add_command(backup_site)
+bench_command.add_command(bench_src)
+bench_command.add_command(config)
+bench_command.add_command(disable_production)
+bench_command.add_command(download_translations)
 bench_command.add_command(exclude_app_for_update)
+bench_command.add_command(get_app)
 bench_command.add_command(include_app_for_update)
+bench_command.add_command(init)
+bench_command.add_command(install)
+bench_command.add_command(migrate_env)
+bench_command.add_command(new_app)
+bench_command.add_command(prepare_beta_release)
+bench_command.add_command(release)
+bench_command.add_command(remote_reset_url)
+bench_command.add_command(remote_set_url)
+bench_command.add_command(remote_urls)
+bench_command.add_command(remove_app)
+bench_command.add_command(renew_lets_encrypt)
+bench_command.add_command(restart)
+bench_command.add_command(retry_upgrade)
+bench_command.add_command(set_default_site)
+bench_command.add_command(set_mariadb_host)
+bench_command.add_command(set_nginx_port)
+bench_command.add_command(set_ssl_certificate_key)
+bench_command.add_command(set_ssl_certificate)
+bench_command.add_command(set_url_root)
+bench_command.add_command(setup)
+bench_command.add_command(shell)
+bench_command.add_command(start)
+bench_command.add_command(switch_to_branch)
+bench_command.add_command(update)
